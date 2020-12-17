4f1a255e856bd4822de94e76937a3f8e33390c12
darnocer <darnocer@gmail.com>
2020-12-17 13:12:50 -0700

    hamburger icon and mobile typewriter font size

--- src/pages/Header.vue
+++ src/pages/Header.vue
@@ -13,7 +13,7 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"><i class="fas fa-bars" style="color:#fff; font-size:28px;"></i></span>
       </button>
       <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
         <ul id="navbar-nav" class="navbar-nav ml-auto">
